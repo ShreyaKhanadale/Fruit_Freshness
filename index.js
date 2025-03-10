1ab7a8cf89a1d22b12bb454a59bc463b2ff03047
ShreyaKhanadale <01fe20bec053@kletech.ac.in>
2025-03-10 22:00:35 +0530

    third

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 function updateLevels() {
     document.getElementById("temp-value").innerText = Math.floor(Math.random() * 10 + 20) + " °C";
     document.getElementById("humidity-value").innerText = Math.floor(Math.random() * 50 + 30) + " %";
-    document.getElementById("gas-value").innerText = Math.floor(Math.random() * 500 + 100) + " PPM";
 }
 function logout() {
     window.location.href = "logout.html"; // Redirects to the logout page
@@ -10,23 +9,27 @@ function logout() {
 
 setInterval(updateLevels, 2000);
 updateLevels();
+
 function getRandomStatus() {
-    const statuses = ["✅ Fresh", "⚠️ Near Expiry", "❌ Expired"];
+    const statuses = ["✅ Fresh",  "❌ Expired"];
     return statuses[Math.floor(Math.random() * statuses.length)];
 }
+function getRandomWastage() {
+    return Math.floor(Math.random() * 5) + " pcs";
+}
+
 function updateInventory() {
     const inventoryData = [
-        { product: "🍎 Apples", quantity: Math.floor(Math.random() * 20), expiry: "03-03-2025", status: getRandomStatus() },
-        { product: "🍌 Bananas", quantity: Math.floor(Math.random() * 20), expiry: "02-03-2025", status: getRandomStatus() },
-        { product: "🍊 Oranges", quantity: Math.floor(Math.random() * 20), expiry: "04-03-2025", status: getRandomStatus() },
-        { product: "🍐 Guava", quantity: Math.floor(Math.random() * 20), expiry: "10-03-2025", status: getRandomStatus() }
-        ];
-        
+        { product: "🍎 Apples", quantity: Math.floor(Math.random() * 20), wastage: getRandomWastage(), status: getRandomStatus() },
+        { product: "🍌 Bananas", quantity: Math.floor(Math.random() * 20), wastage: getRandomWastage(), status: getRandomStatus() },
+        { product: "🍊 Oranges", quantity: Math.floor(Math.random() * 20), wastage: getRandomWastage(), status: getRandomStatus() },
+        { product: "🍐 Guava", quantity: Math.floor(Math.random() * 20), wastage: getRandomWastage(), status: getRandomStatus() }
+    ];
     
     let tableBody = document.getElementById("inventory-table");
     tableBody.innerHTML = "";
     inventoryData.forEach(item => {
-        let row = `<tr><td>${item.product}</td><td>${item.quantity}</td><td>${item.expiry}</td><td>${item.status}</td></tr>`;
+        let row = `<tr><td>${item.product}</td><td>${item.quantity}</td><td>${item.wastage}</td><td>${item.status}</td></tr>`;
         tableBody.innerHTML += row;
     });
 }
